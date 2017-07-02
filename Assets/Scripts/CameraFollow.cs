14c583df573463a07bd8c59523730f4de97996d4
OcarinhaOfTime <vagnerseibert@gmail.com>
2017-07-02 00:58:15 -0700

    Created basic gameplay

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Rigidbody2D target;
+    
+	void LateUpdate () {
+        var pos = transform.position;
+        pos.y = target.position.y;
+        transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/CarMotorController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMotorController : MonoBehaviour {
+    public float max_speed = 100;
+    public float acceleration = 10;
+    public float turn_force = 2;
+
+    private Rigidbody2D rb2d;
+    public Vector2 velocity;
+
+    private void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        var accel = Input.GetAxis("Vertical");
+        var turn = Input.GetAxis("Horizontal");
+        if(rb2d.velocity.y < max_speed)
+            rb2d.AddForce(transform.up * accel * acceleration);
+
+        rb2d.AddTorque(turn * turn_force);
+        velocity = rb2d.velocity;
+    }
+}
